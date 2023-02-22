2e73175cefd14faa74e95e1c2ea09a90592ddf22
ajin3830 <ajin3830@gmail.com>
2023-02-21 23:49:36 -0500

    completed lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
 # your code goes here!
+import time
+# import ipdb
+
+# ipdb.set_trace()
+
+def countdown(num):
+    while num:
+        print(f'{num} SECOND(S)!')
+        num -= 1 
+
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(num):
+    while num:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+        time.sleep(1)
+
+    print('HAPPY NEW YEAR!')
